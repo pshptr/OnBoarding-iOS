4554c19527dfbefa13201c8715e89f170a3fa5d8
pshptr <petia3711@gmail.com>
2025-02-18 23:45:41 +0300

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Pe Tia on 18.02.25.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
